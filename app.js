e6836b252d699bd7d9f42602d0e89304e5aee85d
auditoriumus <51923926+auditoriumus@users.noreply.github.com>
2024-05-07 22:26:08 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let tg = window.Telegram.Webapp;
+let tg = window.Telegram.WebApp;
 
 tg.expand();
 
@@ -15,15 +15,17 @@ let btn5 = document.getElementById("btn5");
 let btn6 = document.getElementById("btn6");
 
 
-btn1.addEventListener("click", function () {
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-    } else {
-        tg.MainButton.setText("Вы выбрали товар 1");
-        item = "1";
-        tg.MainButton.show();
-    }
-})
+if (btn1) {
+    btn1.addEventListener("click", function () {
+        if (tg.MainButton.isVisible) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.setText("Вы выбрали товар 1");
+            item = "1";
+            tg.MainButton.show();
+        }
+    })
+}
 
 Telegram.WebApp.onEvent("MainButtonClicked", function () {
     tg.sendData(item)
@@ -31,7 +33,7 @@ Telegram.WebApp.onEvent("MainButtonClicked", function () {
 
 let usercard = document.getElementById("usercard");
 let p = document.createElement("p");
-p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
+p.innerText = `${tg.initDataUnsafe.first_name}
+${tg.initDataUnsafe.last_name}`;
 
 usercard.appendChild(p);
